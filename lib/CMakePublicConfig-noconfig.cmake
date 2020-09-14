1d076b9a7ab6d19a4166939110115e623de27a52
aafinger <aafinger@iastate.edu>
2020-09-14 09:37:34 -0500

    changed some file destinations, working on target include directories

---
+++ lib/CMakePublicConfig-noconfig.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "CMakePublic::CMakePublic" for configuration ""
+set_property(TARGET CMakePublic::CMakePublic APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CMakePublic::CMakePublic PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "/Users/ally/CMaizeExamples/CMakePublic/lib/libCMakePublic.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS CMakePublic::CMakePublic )
+list(APPEND _IMPORT_CHECK_FILES_FOR_CMakePublic::CMakePublic "/Users/ally/CMaizeExamples/CMakePublic/lib/libCMakePublic.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
